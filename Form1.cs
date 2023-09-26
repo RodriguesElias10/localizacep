6fa5eef8f8134e2a298adbaa9274180a2dcad88c
isaias.bueno <isaias.bueno@informa.solutions>
2023-09-25 23:41:19 -0300

    localizar cep corrigido

--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 using System.Windows.Forms;
+//using Correios.Net;
 
 namespace ApiCep
 {
-    using Correios.Net;
     public partial class Form1 : Form
     {
         public Form1()
@@ -22,28 +17,81 @@ namespace ApiCep
         {
 
         }
-        private void LocalizarCEP()
+
+
+        private async void LocalizarCEP()
         {
-            if (!string.IsNullOrWhiteSpace(txtCEP.Text))
+            try
             {
-                Address endereco = SearchZip.GetAddress(txtCEP.Text);
-                if (endereco.Zip != null)
+                if (!string.IsNullOrWhiteSpace(txtCEP.Text))
                 {
-                    txtEstado.Text = endereco.State;
-                    txtCidade.Text = endereco.City;
-                    txtBairro.Text = endereco.District;
-                    txtEndereco.Text = endereco.Street;
+                    // Formate o CEP para remover caracteres não numéricos
+                    string cep = txtCEP.Text.Replace("-", "").Replace(".", "");
+
+                    // URL da API de consulta de CEP
+                    string apiUrl = $"https://viacep.com.br/ws/{cep}/json/";
+
+                    using (HttpClient client = new HttpClient())
+                    {
+                        HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string json = await response.Content.ReadAsStringAsync();
+                            dynamic responseData = JsonConvert.DeserializeObject(json);
+
+                            if (responseData.cep != null)
+                            {
+                                txtEstado.Text = responseData.uf;
+                                txtCidade.Text = responseData.localidade;
+                                txtBairro.Text = responseData.bairro;
+                                txtEndereco.Text = responseData.logradouro;
+                            }
+                            else
+                            {
+                                MessageBox.Show("CEP não encontrado...");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erro ao consultar o CEP. Verifique sua conexão com a internet.");
+                        }
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Cep não localizado...");
+                    MessageBox.Show("Informe um CEP válido");
                 }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Informe um CEP válido");
+                MessageBox.Show(ex.Message);
             }
         }
+
+        //private void LocalizarCEP()
+        //{
+        //    if (!string.IsNullOrWhiteSpace(txtCEP.Text))
+        //    {
+        //        Address endereco = SearchZip.GetAddress(txtCEP.Text);
+        //        if (endereco.Zip != null)
+        //        {
+        //            txtEstado.Text = endereco.State;
+        //            txtCidade.Text = endereco.City;
+        //            txtBairro.Text = endereco.District;
+        //            txtEndereco.Text = endereco.Street;
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("Cep não localizado...");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Informe um CEP válido");
+        //    }
+        //}
+
         private void cmdBuscar_Click(object sender, EventArgs e)
         {
             LocalizarCEP();
